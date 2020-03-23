d1c93d7c65215b69dcd8dca0481bdebe619d8442
key2-hue <keymaeda3782@gmail.com>
2020-03-23 11:57:03 +0900

    03/23 11:56

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 {
   const lottery = document.getElementById('lottery');
-
+  let flag = 0;
   lottery.addEventListener('click', ()=> {
-    const result = Math.random();
-    if(result < 0.20) {
-      lottery.textContent = "Lucky!";
-    } else if(result < 0.70) {
-      lottery.textContent = "SoSo";
-    } else {
-      lottery.textContent = "Bad...";
+    if(flag === 1) {
+      return;
     }
+    flag = 1;
+    const result = Math.random();
     const resultLottery = document.getElementById('result');
+    if(result < 0.20) {
+      resultLottery.innerHTML = '<p>Lucky!</p>';
+    } else if(result < 0.70) {
+      resultLottery.innerHTML = '<p>Soso</p>';
+    } else {
+      resultLottery.innerHTML = '<p>Bad...</p>';
+    }
+    
     resultLottery.classList.remove('inactive');
     resultLottery.classList.add('active');
 
+  });
+
+  document.getElementById('retry').addEventListener('click', ()=> {
+    flag = 0;
+    const resultLottery = document.getElementById('result');
+    resultLottery.classList.remove('active');
+    resultLottery.classList.add('inactive');
+    resultLottery.innerHTML = "";
   });
 }
